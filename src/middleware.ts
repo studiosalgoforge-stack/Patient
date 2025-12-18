66fb92c116da2dada674a9f42ee684f1c4ab9611
samiya2804 <samiyaa2804@gmail.com>
2025-12-18 19:05:23 +0530

    changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,11 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+// This explicitly exports the middleware function that Next.js is looking for
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
   matcher: [
